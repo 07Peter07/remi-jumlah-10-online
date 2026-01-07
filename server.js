4cda873dbb4a77dbba0b5a62baa15843cd9e0c4f
Peter <petruspardede07@gmail.com>
2026-01-07 12:28:25 +0700

    feat: create deck and deal cards

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.use(express.static("public"));
 
 // SOCKET LOGIC
 const rooms = {};
+const SUITS = ["♥", "♦", "♠", "♣"];
+const VALUES = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+
+function createDeck() {
+  const deck = [];
+  SUITS.forEach(s =>
+    VALUES.forEach(v => deck.push({ v, s }))
+  );
+  return deck.sort(() => Math.random() - 0.5);
+}
 
 io.on("connection", socket => {
   console.log("socket connected:", socket.id);
@@ -23,19 +33,28 @@ io.on("connection", socket => {
     socket.join(roomId);
 
     if (!rooms[roomId]) {
-      rooms[roomId] = {
-        players: [],
-        table: [],
-        turn: 0
-      };
-    }
+  const deck = createDeck();
+
+  const table = deck.splice(0, 10);
+
+  rooms[roomId] = {
+    deck,
+    table,
+    players: [],
+    turn: 0
+  };
+}
+
 
     const playerIndex = rooms[roomId].players.length;
 
-    rooms[roomId].players.push({
-      id: socket.id,
-      hand: []
-    });
+    const hand = rooms[roomId].deck.splice(0, 7);
+
+rooms[roomId].players.push({
+  id: socket.id,
+  hand
+});
+
 
     socket.emit("joined", {
       playerIndex,
